7f3ec190c1f9378f3ae7427af897f438679904fe
Roshan <nkroshankumar@gmail.com>
2022-01-29 00:23:13 +0530

    Logo size modified

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
   <header >
       <nav className="flex justify-between items-center p-2 md:px-4 bg-gradient-to-r from-[#A5FECB] via-[#20BDFF] to-[#5433FF]/80 shadow-lg shadow-black/40">
-          <img src={logo} alt="TravelIde_logo" className='w-20 h-12 md:w-26 md:h-20' />
+          <img src={logo} alt="TravelIde_logo" className='w-26 h-12 md:w-26 md:h-20' />
       </nav>
     </header>)
 };
